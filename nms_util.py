0a60f0b9e2797e2e049cfb91efd2bb06a18fae4c
NathanOD <nathan.odic@mines-ales.org>
2022-06-28 16:14:47 +0200

    IoU

--- nms_util.py
+++ nms_util.py
@@ -1,20 +1,21 @@
 import numpy as np
 import tensorflow as tf
-"""
-def IoU(b1, b2):
-    Ix = max(b1[0], b2[0])
-    Iy = max(b1[1], b2[1])
-    Ixp = min(b1[0] + b1[2], b2[0] + b2[2])
-    Iyp = min(b1[1] + b1[3], b2[1] + b2[3])
-    I = (Ixp - Ix) * (Iyp - Iy)
-    return 1 / ( (b1[2] * b1[3] + b2[2] * b2[3]) / I - 1)
-"""
-def IoU(b1,b2):
-    
-    inter = np.logical_and([b1[0],b1[1],b1[0]+b1[2],b1[1]+b1[3]],[b2[0],b2[1],b2[0]+b2[2],b2[1]+b2[3]])
-    union = np.logical_or([b1[0],b1[1],b1[0]+b1[2],b1[1]+b1[3]],[b2[0],b2[1],b2[0]+b2[2],b2[1]+b2[3]])
-    print(np.sum(inter)/np.sum(union))
-    return np.sum(inter)/np.sum(union)
+
+def IoU(box1, box2) :
+    x1 = max(box1[0], box2[0])
+    y1 = max(box1[1], box2[1])
+    x2 = min(box1[2], box2[2])
+    y2 = min(box1[3], box2[3])
+    width = (x2 - x1)
+    height = (y2 - y1)
+    if (width<0) or (height <0):
+        return 0.0
+    area_overlap = width * height
+    area1 = (box1[2] - box1[0]) * (box1[3] - box1[1])
+    area2 = (box2[2] - box2[0]) * (box2[3] - box2[1])
+    area_combined = area1 + area2 - area_overlap
+    iou = area_overlap / area_combined
+    return iou
 
 def NMS(boxes, classes, scores):
     clBoxes = []
