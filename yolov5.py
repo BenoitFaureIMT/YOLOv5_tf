857a77c8d886ed86da1f94557e6e482a6f9bf2e8
BenoitFaureIMT <Dovahkiin2601>
2022-06-28 12:04:10 +0200

    ca marche ish

--- yolov5.py
+++ yolov5.py
@@ -1,11 +1,24 @@
 import tensorflow as tf
+from PIL import Image
+import numpy as np
 
 class YOLOv5(object):
     def __init__(self, tf_lite_f_path):
         self.interpreter = tf.lite.Interpreter(tf_lite_f_path)
-        print("Input : ", self.interpreter.get_input_details())
-        print("Output : ", self.interpreter.get_output_details())
+        input_details = self.interpreter.get_input_details()
+        output_details = self.interpreter.get_output_details()
+        self.interpreter.allocate_tensors()
+        print("Input : ", input_details)
+        print("Output : ", output_details)
 
-        
+        img = Image.open("chien.webp").resize((640, 640), Image.ANTIALIAS)
+        img = tf.keras.preprocessing.image.img_to_array(img)
+
+        self.interpreter.set_tensor(input_details[0]['index'], tf.expand_dims(img, axis=0))
+        self.interpreter.invoke()
+
+        output_data = self.interpreter.get_tensor(output_details[0]['index'])
+        print(output_data)
+
 
 YOLOv5("test.tflite")
