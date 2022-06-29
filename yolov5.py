e64cab3f44ff5b8d61cc2a7f3849c55aa2953337
BenoitFaureIMT <Dovahkiin2601>
2022-06-29 12:27:33 +0200

    ca marche!!!!!!!!

--- yolov5.py
+++ yolov5.py
@@ -9,10 +9,13 @@ import time
 from nms_util import NMS
 
 class YOLOv5(object):
-    def __init__(self, tf_lite_f_path):
+    def __init__(self, path):
+        self.model = tf.saved_model.load(path)
+    
+    def load_tflite(self, tf_lite_f_path):
         self.interpreter = tf.lite.Interpreter(tf_lite_f_path)
-
-    def run_net(self, img):
+    
+    def run_tflite(self, img):
         #Get interpreter info
         input_details = self.interpreter.get_input_details()
         output_details = self.interpreter.get_output_details()
@@ -27,36 +30,51 @@ class YOLOv5(object):
 
         output_data = self.interpreter.get_tensor(output_details[0]['index'])
         return output_data
+
+    def run_net(self, img):
+        #Process image
+        img = tf.keras.preprocessing.image.img_to_array(img)
+        img = tf.convert_to_tensor(img) / 225
+        img = img[tf.newaxis, ...]
+
+        output_data = self.model(img).numpy()
+
+        return output_data
     
     def process_output(self, output_data, min_score = 0.5):
 
         #Output shape : (Excluding first dimension) -> [..., [x, y, w, h, confidence, ind0 conf, ind1 conf, ...], ...]
         #output_data = [[bbox[0] - bbox[2] / 2, bbox[1] - bbox[3] / 2, bbox[0] + bbox[2] / 2, bbox[1] + bbox[3] / 2, bbox[4], bbox[5:].argmax()] for bbox in output_data[0]]
-        output_data = [[bbox[1] - bbox[3] / 2, bbox[0] - bbox[2] / 2, bbox[1] + bbox[3] / 2, bbox[0] + bbox[2] / 2, bbox[4], bbox[5:].argmax()] for bbox in output_data[0]]
+        #output_data = [[bbox[1] - bbox[3] / 2, bbox[0] - bbox[2] / 2, bbox[1] + bbox[3] / 2, bbox[0] + bbox[2] / 2, bbox[4], bbox[5:].argmax()] for bbox in output_data[0]]
+        output_data = output_data[0]
 
         #Elimination
-        i = 0
-        for _ in range(len(output_data)):
-            if(output_data[i][4] < min_score):
-                del output_data[i]
-            else:
-                i += 1
+        # i = 0
+        # for _ in range(len(output_data)):
+        #     if(output_data[i][4] < min_score):
+        #         np.delete(output_data, i) #del output_data[i]
+        #     else:
+        #         i += 1
+        output_data = output_data[np.where(output_data[:, 4] > min_score)]
+        
+        # o = np.copy(output_data)
+        # output_data[:, 1] = o[:, 0] - o[:, 2] / 2  # top left x
+        # output_data[:, 0] = o[:, 1] - o[:, 3] / 2  # top left y
+        # output_data[:, 3] = o[:, 0] + o[:, 2] / 2  # bottom right x
+        # output_data[:, 2] = o[:, 1] + o[:, 3] / 2  # bottom right y
+        output_data = [[bbox[1] - bbox[3] / 2, bbox[0] - bbox[2] / 2, bbox[1] + bbox[3] / 2, bbox[0] + bbox[2] / 2, bbox[4], bbox[5:].argmax()] for bbox in output_data]
 
         #NMS    
         #output_data = NMS(output_data)
         output_data = np.array(output_data)
-        a = tf.convert_to_tensor(output_data[:, :4], dtype=tf.float32)
-        b = tf.convert_to_tensor([v[4] for v in output_data], dtype=tf.float32)
-        selected = tf.image.non_max_suppression(a, b, 30000, 0.65)
-        out = []
-        for i in selected:
-            out.append(output_data[i])
+        selected = tf.image.non_max_suppression(output_data[:, :4], output_data[:, 4], 30000, 0.65)
+        output_data = output_data[selected, :]
 
-        return np.array(out)
+        return output_data
 
     #Utility functions
     def run_img(self, img_path):
-        return self.run_net(Image.open(img_path).resize((640, 640), Image.ANTIALIAS)) #TODO:Probably remove antialias cause deperecated
+        return self.run_net(cv2.resize(cv2.imread(img_path), (640, 640), interpolation=cv2.INTER_LINEAR))
     
     def warm_up(self):
         self.run_net(tf.zeros((640, 640, 3)))
@@ -78,36 +96,14 @@ class YOLOv5(object):
         cv2.imshow('BBox', img)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
-    
-    #----------------------------------------------------------------Experimental----------------------------------------------------------------
-    def load_saved_model(self, path):
-        self.model = tf.saved_model.load(path)
-    
-    def run_experimental(self, im):
-        y = self.model(im).numpy()
-        return y
 
-tflite = False
-yolo_model = YOLOv5("test.tflite")
+yolo_model = YOLOv5("saved_model")
 
-yolo_model.load_saved_model("saved_model")
-#Process image
-img = tf.keras.preprocessing.image.img_to_array(cv2.imread("test.jpg"))
-img = tf.convert_to_tensor(img)
-img = img[tf.newaxis, ...]
-yolo_model.run_experimental(img)
+yolo_model.warm_up()
 
 t = time.perf_counter()
-yolo_model.run_experimental(img)
+output_data = yolo_model.detect("test.jpg", 0.5)
 t -= time.perf_counter()
 print(-t)
 
-if tflite:
-    yolo_model.warm_up()
-
-    t = time.perf_counter()
-    output_data = yolo_model.detect("test.jpg", 0.8)
-    t -= time.perf_counter()
-    print(-t)
-
-    yolo_model.display(output_data, "test.jpg")
+yolo_model.display(output_data, "test.jpg")
